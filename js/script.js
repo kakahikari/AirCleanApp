d65d431a03fc360267e074b8a1e1c748dfba6e17
kakahikari <kakahikari@gmail.com>
2016-08-18 23:51:24 +0800

    mobile test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-var a = 1;
+$(function () {
+    $(".container").touchwipe({
+        wipeLeft: function () { alert("left"); },
+        wipeRight: function () { alert("right"); },
+        wipeUp: function () { alert("up"); },
+        wipeDown: function () { alert("down"); },
+        min_move_x: 20,
+        min_move_y: 20,
+        preventDefaultEvents: true
+    });
+});
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,1 +1,13 @@
-var a = 1;
+declare var $: any;
+
+$(function() {
+  $(".container").touchwipe({
+     wipeLeft: function() { alert("left"); },
+     wipeRight: function() { alert("right"); },
+     wipeUp: function() { alert("up"); },
+     wipeDown: function() { alert("down"); },
+     min_move_x: 20,
+     min_move_y: 20,
+     preventDefaultEvents: true
+  });
+} );
